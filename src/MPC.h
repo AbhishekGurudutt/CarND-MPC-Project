65fe89f599d9840f2f7b92dcd86cee99c5eb2762
Abhishek Gurudurtt <abhishekgurudurtt@Abhisheks-MacBook-Pro.local>
2018-02-17 14:07:48 -0500

    completed the project

--- src/MPC.h
+++ src/MPC.h
@@ -6,8 +6,14 @@
 
 using namespace std;
 
+const size_t N = 10;
+const double dt = 0.1;
+
+
 class MPC {
  public:
+
+	double prev_throttle = 0.0;
   MPC();
 
   virtual ~MPC();
@@ -15,6 +21,9 @@ class MPC {
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
   vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+  const double Lf = 2.67;
+
 };
 
 #endif /* MPC_H */
